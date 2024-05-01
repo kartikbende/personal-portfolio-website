d981151330ac36c68d838e3d4f32784e954f1a0e
kartikbende <80601764+kartikbende@users.noreply.github.com>
2024-05-02 02:00:21 +0530

    added the homescreen

--- mywebsite/lib/main.dart
+++ mywebsite/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mywebsite/pages/homescreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,8 +9,11 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
       title: 'Kartik Bende',
+      home: HomePage(),
     );
   }
 }
