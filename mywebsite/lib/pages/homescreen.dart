e3d31427e0edb4fa1414b5e849b3518fcd51c4ea
kartikbende <80601764+kartikbende@users.noreply.github.com>
2024-05-02 12:37:42 +0530

    miniupdate

--- mywebsite/lib/pages/homescreen.dart
+++ mywebsite/lib/pages/homescreen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mywebsite/constants/colors.dart';
-import 'package:mywebsite/widgets/header-desktop.dart';
+import 'package:mywebsite/widgets/sitelogo.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,7 +18,23 @@ class _HomePageState extends State<HomePage> {
           scrollDirection: Axis.vertical,
           children: [
             //main
-            const HeaderDesktop(),
+            // const HeaderDesktop(),
+            Container(
+              height: 50.0,
+              margin: const EdgeInsets.fromLTRB(40, 5, 20, 5),
+              child: Row(
+                children: [
+                  SiteLogo(
+                    onTap: () {},
+                  ),
+                  const Spacer(),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.menu),
+                  )
+                ],
+              ),
+            ),
             //Skills
             Container(
               height: 500,
